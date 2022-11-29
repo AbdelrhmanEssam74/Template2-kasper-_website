018fba6b68cc2af22917cf9edb249e840dd38e9c
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2022-11-29 23:16:34 +0200

    Update parallax.js

--- js/parallax.js
+++ js/parallax.js
@@ -1,13 +1,6 @@
 ///////////////////////
-/* if (document.body.clientWidth >= 1000) {
+if (document.body.clientWidth >= 1000) {
   window.onscroll = () => {
-    let portfolio = document.querySelector(".imgs-container");
-    const rect2 = portfolio.getBoundingClientRect();
-    if (rect2.top >= -550.875 && rect2.top <= 495.875) {
-      portfolio.style.opacity = "1";
-    } else {
-      portfolio.style.opacity = "0";
-    }
     let designImag = document.querySelector(".image");
     const rect = designImag.getBoundingClientRect();
     if (rect.top >= -300.875 && rect.top <= 300.875) {
@@ -17,34 +10,40 @@
       designImag.style.right = "510px";
     }
   };
-} */
-
-//increasing number
-
-let nums = document.querySelectorAll(".box .number");
-
-let section = document.querySelector(".stats");
-
-let started = false;
-
-window.onscroll = function () {
-  if (window.scrollY >= section.offsetTop) {
-    if (!started) {
-      nums.forEach((num) => startCount(num));
-    }
-    started = true;
-  }
-};
-
-function startCount(el) {
-  let goal = el.dataset.goal;
-  let counter = setInterval(() => {
-    el.textContent++;
-    if (el.textContent == goal) {
-      clearInterval(counter);
-    }
-  }, 10);
 }
 
-console.log(window.scrollY)
-console.log(section.offsetTop)
+
+
+
+//Portfolio img
+const box_img = document.querySelectorAll(".img_box");
+console.log(box_img)
+const observer2 = new IntersectionObserver((ens) => {
+  ens.forEach((en) => {
+    console.log(en);
+    if (en.isIntersecting) {
+      en.target.classList.add("box_img_show");
+    } else {
+      en.target.classList.remove("box_img_show");
+    }
+  });
+});
+
+box_img.forEach((el) => observer2.observe(el));
+
+
+
+//all h2 of sections
+const heading = document.querySelectorAll(".heading2");
+const observer = new IntersectionObserver((ens) => {
+  ens.forEach((en) => {
+    if (en.isIntersecting) {
+      en.target.classList.add("show");
+    } else {
+      en.target.classList.remove("show");
+    }
+  });
+});
+
+heading.forEach((el) => observer.observe(el));
+
